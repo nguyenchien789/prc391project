a1444e00859631be0f8d3c86e93b59dbfb0d16a6
nguyenchien789 <52992835+nguyenchien789@users.noreply.github.com>
2020-12-16 19:44:47 +0700

    Add project files.

---
+++ prc391project/Controllers/ClassesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using prc391project.Models;
+
+namespace prc391project.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private readonly PRC391Context _context;
+
+        public ClassesController(PRC391Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Classes
+        public async Task<IActionResult> Index()
+        {
+            var pRC391Context = _context.Class.Include(c => c.Course).Include(c => c.User);
+            return View(await pRC391Context.ToListAsync());
+        }
+
+        // GET: Classes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .Include(c => c.Course)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // GET: Classes/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId");
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CourseId,UserId")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@class);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId", @class.CourseId);
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId", @class.UserId);
+            return View(@class);
+        }
+
+        // GET: Classes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId", @class.CourseId);
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId", @class.UserId);
+            return View(@class);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CourseId,UserId")] Class @class)
+        {
+            if (id != @class.CourseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@class);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassExists(@class.CourseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId", @class.CourseId);
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId", @class.UserId);
+            return View(@class);
+        }
+
+        // GET: Classes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .Include(c => c.Course)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @class = await _context.Class.FindAsync(id);
+            _context.Class.Remove(@class);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassExists(int id)
+        {
+            return _context.Class.Any(e => e.CourseId == id);
+        }
+    }
+}
---
+++ prc391project/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prc391project.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Class = new HashSet<Class>();
+            Course = new HashSet<Course>();
+        }
+
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public bool? Role { get; set; }
+        public string Email { get; set; }
+
+        public virtual ICollection<Class> Class { get; set; }
+        public virtual ICollection<Course> Course { get; set; }
+    }
+}
